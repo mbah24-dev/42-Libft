84337a3bcebaf3de3c3637e6a1e37a75d561f502
BAH Mamad <mbah@student.42lyon.fr>
2024-10-09 23:38:10 +0200

    feat: start building ft_itoa - prepping the int to string magic Initial work on ft_itoa: setting up the basics for converting integers to strings. Got the skeleton in place, just missing the special cases and edge handling. 🚀

---
+++ source/ft_itoa.c
@@ -0,0 +1,71 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbah <mbah@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/09 11:22:25 by mbah              #+#    #+#             */
+/*   Updated: 2024/10/09 11:41:35 by mbah             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#include "libft.h"
+
+static int	ft_itoa_len(long nbr)
+{
+	int		len;
+
+	len = 1;
+	if (nbr < 0)
+		len++;
+	while (nbr / 10 != 0)
+	{
+		nbr /= 10;
+		len++;
+	}
+	return (len);
+}
+
+static void	ft_itoa_recursive(char *str, int *i, long nbr)
+{
+	if (nbr >= 10)
+	{
+		ft_itoa_recursive(str, i, nbr / 10);
+		ft_itoa_recursive(str, i, nbr % 10);
+	}
+	else
+	{
+		str[*i] = nbr + 48;
+		(*i)++;
+	}
+}
+
+/**
+ * Alloue et retourne une chaine de caractères représentant 
+ * l’integer reçu en argument.
+ *
+ * \param	n	L'entier signé à convertir
+ * \return		La chaine de caractères représentant l’entier signé.
+ */
+char	*ft_itoa(int n)
+{
+	char	*res;
+	int		i;
+	long	temp;
+
+	temp = n;
+	res = (char *) ft_calloc(ft_itoa_len(temp) + 1, 1);
+	if (!res)
+		return (NULL);
+	i = 0;
+	if (temp < 0)
+	{
+		res[i++] = '-';
+		temp *= -1;
+	}
+	ft_itoa_recursive(res, &i, temp);
+	res[i] = 0;
+	return (res);
+}
