17c802ec22066a47f6633f722a29a5493864a6fa
BAH Mamad <mbah@student.42lyon.fr>
2024-11-23 13:57:27 +0100

    Implémentation de ft_atoi

---
+++ ft_atoi.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbah <mbah@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/07 20:10:33 by mbah              #+#    #+#             */
+/*   Updated: 2024/11/18 19:09:39 by mbah             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static int	ft_isblank(char c)
+{
+	return ((c >= 9 && c <= 13) || c == 32);
+}
+
+int	ft_atoi(const char *str)
+{
+	size_t	i;
+	int		res;
+	int		sign;
+
+	i = 0;
+	res = 0;
+	sign = 1;
+	while (ft_isblank(str[i]))
+		i++;
+	if (str[i] == '+' || str[i] == '-')
+	{
+		if (str[i] == '-')
+			sign = -1;
+		i++;
+	}
+	while (str[i] && ft_isdigit(str[i]))
+	{
+		res = res * 10 + (str[i] - 48);
+		i++;
+	}
+	return (res * sign);
+}
