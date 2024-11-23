e805b96493512f5710752c327e355fc5c4785b9e
BAH Mamad <mbah@student.42lyon.fr>
2024-11-23 13:57:28 +0100

    Implémentation de ft_substr

---
+++ ft_substr.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbah <mbah@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/07 21:41:13 by mbah              #+#    #+#             */
+/*   Updated: 2024/11/13 17:30:44 by mbah             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*res;
+
+	if (!s)
+		return (NULL);
+	if (start > ft_strlen(s))
+		return (ft_strdup(""));
+	if (len > (ft_strlen(s + start)))
+		len = ft_strlen(s + start);
+	res = (char *) malloc((len + 1) * sizeof(char));
+	if (!res)
+		return (NULL);
+	ft_strlcpy(res, s + start, len + 1);
+	return (res);
+}
